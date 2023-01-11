0b983bdeadbb13397f090cc5c3121387e9d81b19
sumcoinlabs <sumcoinindex@gmail.com>
2023-01-11 08:25:29 -0700

    hex IP's

--- src/net.cpp
+++ src/net.cpp
@@ -1263,12 +1263,10 @@ void ThreadDNSAddressSeed()
 
 
 
-// Physical IP seeds: 32-bit IPv4 addresses: e.g. 178.33.22.32 = 0x201621b2
+// Physical IP seeds: 32-bit IPv4 addresses: e.g. 178.33.22.32 = 0x201621b2 // hex is in reverse because sunny is a dumbshit
 unsigned int pnSeed[] =
 {
-    0x36a3b545, 0x3c1c26d8, 0x4031eb6d, 0x4d3463d1, 0x586a6854, 0x5da9ae65,
-    0x6deb7318, 0x9083fb63, 0x961bf618, 0xcabd2e4e, 0xcb766dd5, 0xdd514518,
-    0xdff010b8, 0xe9bb6044, 0xedb24a4c,
+    0x0900A8C0, 0x1C00A8C0, 0x0100007F, 
 };
 
 void DumpAddresses()
