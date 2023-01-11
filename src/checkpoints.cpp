6ce576a68acd1092cbc6c1c29cc9aa8180361715
Sumcoin <37975862+sumcoinlabs@users.noreply.github.com>
2023-01-11 07:53:08 -0700

    update CCheckpointData

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -45,7 +45,7 @@ namespace Checkpoints
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1345090300, // * UNIX timestamp of last checkpoint block
+        1522578000, // * UNIX timestamp of last checkpoint block
         0,   // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         0     // * estimated number of transactions per day after checkpoint
@@ -60,7 +60,7 @@ namespace Checkpoints
         ;
     static const CCheckpointData dataTestnet = {
         &mapCheckpointsTestnet,
-        1345090300,
+        1522578000,
         0,
         0
     };
