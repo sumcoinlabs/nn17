2dd3b04b6e1d38378b96b242d15e4e3540464120
sumcoinlabs <sumcoinindex@gmail.com>
2023-01-11 14:59:11 -0700

    set block reward

--- src/main.h
+++ src/main.h
@@ -65,7 +65,7 @@ static const int64 MIN_TX_FEE_PREV7 = CENT;
 static const int64 MIN_TX_FEE = CENT / 10;
 static const int64 MIN_RELAY_TX_FEE = CENT / 10;
 static const int64 PERKB_TX_FEE = CENT;
-static const int64 MAX_MINT_PROOF_OF_WORK = 200000; // 200,000 for the initial PoW;
+static const int64 MAX_MINT_PROOF_OF_WORK = 10000000; // 10,000,000 for each PoW block - 20 total;
 static const int64 MIN_TXOUT_AMOUNT = CENT;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY_PPC = 500;
