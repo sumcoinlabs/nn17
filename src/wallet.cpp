3755927104bdaffdc346f2bef12d60a6b7877e99
sumcoinlabs <sumcoinindex@gmail.com>
2023-01-11 16:13:26 -0700

    remove log print

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1546,7 +1546,7 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
         int64 nReward = GetProofOfStakeReward(nCoinAge);
         // Refuse to create mint that has zero or negative reward
         if(nReward <= 0) {
-        LogPrintf("Reward for PoS is 0. Continuing\n");
+        //LogPrintf("Reward for PoS is 0. Continuing\n");
       //  LogPrintf("nCredit=%d, nReward=%d\n", nCredit, nReward);
           return false;
         }
