e8ecc85163bb440989efb947558af2cee555f38a
sumcoinlabs <sumcoinindex@gmail.com>
2023-02-10 12:43:31 -0700

    30 sec block spacing

--- src/main.h
+++ src/main.h
@@ -80,7 +80,7 @@ static const int64 MIN_TXOUT_AMOUNT = CENT;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY_PPC = 100;
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
-static const int STAKE_TARGET_SPACING = 1 * 60; // 1-minute block spacing
+static const int STAKE_TARGET_SPACING = 30; // 30 second block spacing
 static const int STAKE_MIN_AGE = 60 * 60 * 24 * 1; // minimum age for coin age
 static const int STAKE_MAX_AGE = 60 * 60 * 24 * 90; // stake age of full weight
 /** Maximum number of script-checking threads allowed */
